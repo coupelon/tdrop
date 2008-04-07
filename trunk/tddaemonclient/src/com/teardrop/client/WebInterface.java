4d9725236122a5f80b18594ce2aaecf3396a45b4
olivier <olivier@e14bd3a3-8b4b-0410-91c7-317e00807df5>
2008-04-07 17:00:42 +0000

    Seperated the EngineTree creation from the main code. Slightly cleaned the Web interface.

--- trunk/tddaemonclient/src/com/teardrop/client/WebInterface.java
+++ trunk/tddaemonclient/src/com/teardrop/client/WebInterface.java
@@ -1,63 +1,24 @@
 package com.teardrop.client;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.http.client.*;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
-
-import com.google.gwt.json.client.*;
-
-//import com.gwtext.client.data.Node;
 import com.gwtext.client.widgets.tree.*;
-//import com.gwtext.client.widgets.tree.event.TreeNodeListenerAdapter;
+import com.gwtext.client.widgets.Button;
+import com.gwtext.client.widgets.event.ButtonListenerAdapter;
+import com.gwtext.client.core.EventObject;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class WebInterface implements EntryPoint {
-	  /*
-	   * Class for handling the fetch button's click event.
-	   */
-	  private class SearchButtonClickListener implements ClickListener {
-	    public void onClick(Widget sender) {
-	      engTree.setVisible(false);
-	      doFetchURL();
-	    }
-	  }
-
-	  /*
-	   * Default URL to use to fetch JSON objects. Note that the contents of this
-	   * JSON result were as a result of requesting the following URL:
-	   * 
-	   * http://api.search.yahoo.com/ImageSearchService/V1/imageSearch?appid=YahooDemo&query=potato&results=2&output=json
-	   * 
-	   */
-	  //private static final String DEFAULT_SEARCH_URL = "http://localhost:8080/request_tree";
-	  private static final String DEFAULT_SEARCH_URL = "search-results.js";
-
-	  /*
-	   * Text displayed on the fetch button when we are in a default state.
-	   */
 	  private static final String SEARCH_BUTTON_DEFAULT_TEXT = "Search";
-
-	  /*
-	   * Text displayed on the fetch button when we are waiting for a JSON reply.
-	   */
-	  private static final String SEARCH_BUTTON_WAITING_TEXT = "Waiting for JSON Response...";
-
-	  private static final String ICON_PATH = "teardrop/";
-	  
-	  //private Tree jsonTree = new Tree();
-	  
-	  private TreePanel engTree = new TreePanel();
-
-	  private Button searchButton = new Button();
+	  private TreePanel engTree = new EngineTree();
+	  private Button searchButton = new Button(SEARCH_BUTTON_DEFAULT_TEXT,new ButtonListenerAdapter() {  
+	      public void onClick(Button button, EventObject e) {
+	    	  //Handle the button click
+		      }  
+		    });
 
 	  /**
 	   * Entry point for this simple application. Currently, we build the
@@ -66,142 +27,6 @@ public class WebInterface implements EntryPoint {
 	  public void onModuleLoad() {
 	    initializeMainForm();
 	  }
-	  
-	  /*
-	   * Add the object presented by the JSONValue as a children to the requested
-	   * TreeItem.
-	   */
-	  private void generateEngineTree(TreeNode treeItem, JSONValue jsonValue) {	    
-	    JSONValue categ;
-	    if ((categ = getJSONSet(jsonValue,"categories")) != null) {
-	    	JSONArray categArray;
-	    	if ((categArray = categ.isArray()) != null) {
-      	      for (int i = 0; i < categArray.size(); ++i) {
-      	    	TreeNode treeCat = new TreeNode(getJSONSetValue(categArray.get(i), "name"));
-//      	    	treeCat.addListener(new TreeNodeListenerAdapter() {
-//      	    		public void onCheckChanged(Node node, boolean checked) {
-//      	    		    TreeNode[] children = (TreeNode[]) node.getChildNodes();
-//      	    			for(int j = 0; j < children.length; ++j) {
-//      	    				children[j].setChecked(((TreeNode)node).getUI().isChecked());
-//      	    			}
-//      	    		}
-//      	    	});
-      	    	treeCat.setIcon(ICON_PATH + getJSONSetValue(categArray.get(i), "icon"));
-      	    	treeCat.setChecked(false);
-      	    	treeItem.appendChild(treeCat);
-      	        JSONValue engin;
-      	        if ((engin = getJSONSet(categArray.get(i),"engines")) != null) {
-	  	        	JSONArray enginArray;
-	  	        	if ((enginArray = engin.isArray()) != null) {
-	  	    	      for (int j = 0; j < enginArray.size(); ++j) {
-	  	    	    	TreeNode treeEng;
-	  	    	    	if (getJSONSetValue(enginArray.get(j),"title").equals(""))
-	  	    	    		treeEng = new TreeNode(getJSONSetValue(enginArray.get(j),"name"));
-	  	    	    	else 
-	  	    	    		treeEng = new TreeNode(getJSONSetValue(enginArray.get(j),"title"));
-	  	    	    	treeEng.setIcon(ICON_PATH + getJSONSetValue(enginArray.get(j),"icon"));
-	  	    	    	treeEng.setAttribute("name", getJSONSetValue(enginArray.get(j),"name"));
-	  	    	    	treeEng.setChecked(false);
-	  	    	    	treeCat.appendChild(treeEng);
-	  	    	      }
-	  	    	    }
-      	        }
-      	      }
-      	    }
-	    }
-	  }
-	  
-	  private String getJSONSetValue(JSONValue jsonValue, String name) {
-	    JSONObject jsonObject;
-	    JSONString jsonString;
-		if ((jsonObject = jsonValue.isObject()) != null) {
-	      Set keys = jsonObject.keySet();
-	      for (Iterator iter = keys.iterator(); iter.hasNext();) {
-	        String key = (String) iter.next();
-	        if(key.equals(name))
-		        if ((jsonString = jsonObject.get(key).isString()) != null) {
-		  	      return jsonString.stringValue();
-		  	    }
-	      }
-	    }
-		return null;
-	  }
-	  
-	  private JSONValue getJSONSet(JSONValue jsonValue, String name) {
-		JSONObject jsonObject;
-		if ((jsonObject = jsonValue.isObject()) != null) {
-	      Set keys = jsonObject.keySet();
-	      for (Iterator iter = keys.iterator(); iter.hasNext();) {
-	        String key = (String) iter.next();
-	        if(key.equals(name))
-	        	return jsonObject.get(key);
-	      }
-	    }
-		return null;
-	  }
-
-	  private void displayError(String responseText) {
-	    engTree.clear();
-	    engTree.setVisible(true);
-	    TreeNode treeItem = new TreeNode("Failed to parse JSON response"); 
-	    engTree.setRootNode(treeItem);
-	  }
-
-	  /*
-	   * Update the treeview of a JSON object.
-	   */
-	  private void displayJSONObject(JSONValue jsonValue) {
-		engTree.setVisible(true);
-	    //jsonTree.removeItems();
-	    //jsonTree.setVisible(true);
-	    //TreeItem treeItem = jsonTree.addItem("JSON Response");
-	    generateEngineTree(engTree.getRootNode(), jsonValue);
-	    //treeItem.setStyleName("JSON-JSONResponseObject");
-	    //treeItem.setState(true);
-	  }
-
-	  /*
-	   * Fetch the requested URL.
-	   */
-	  private void doFetchURL() {
-	    searchButton.setText(SEARCH_BUTTON_WAITING_TEXT);
-	    RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(DEFAULT_SEARCH_URL));
-
-	    
-	    try {
-	    	builder.sendRequest(null, new RequestCallback() {
-	    	    public void onError(Request request, Throwable exception) {
-	    	    	displayError("Couldn't connect to server (could be timeout, SOP violation, etc.)");     
-	    	    }
-
-	    	    public void onResponseReceived(Request request, Response response) {
-	    	      if (200 == response.getStatusCode()) {
-	    	    	  try {
-	    	  	        JSONValue jsonValue = JSONParser.parse(response.getText());
-	    	  	        displayJSONObject(jsonValue);
-	    	  	      } catch (JSONException e) {
-	    	  	        displayError(response.getText());
-	    	  	      }
-	    	  	      searchButton.setText(SEARCH_BUTTON_DEFAULT_TEXT);
-	    	          // Process the response in response.getText()
-	    	      } else {
-	    	    	    displayError(response.getStatusText());
-						// Handle the error.  Can get the status text from response.getStatusText()
-						searchButton.setText(SEARCH_BUTTON_DEFAULT_TEXT);
-	    	      }
-	    	    }       
-	    	  });
-	    	} catch (RequestException e) {
-	    	  // Couldn't connect to server        
-	    	}
-	  }
-
-	  /*
-	   * Causes the text of child elements to wrap.
-	   */
-//	  private String getChildText(String text) {
-//	    return "<span style='white-space:normal'>" + text + "</span>";
-//	  }
 
 	  /**
 	   * Initialize the main form's layout and content.
@@ -209,23 +34,6 @@ public class WebInterface implements EntryPoint {
 	  private void initializeMainForm() {
 	    searchButton.setStyleName("JSON-SearchButton");
 	    searchButton.setText(SEARCH_BUTTON_DEFAULT_TEXT);
-	    searchButton.addClickListener(new SearchButtonClickListener());
-
-	    TreeNode aroot = new TreeNode("categories"); 
-		engTree.setRootNode(aroot);
-		// Avoids showing an "empty" cell
-	    engTree.setVisible(false);
-	    engTree.setHeaderAsText(true);
-	    engTree.setTitle("Categories");
-	    engTree.setCollapsible(true);
-	    engTree.setHeight(400);  
-	    engTree.setWidth(200);  
-	    engTree.setAnimate(true);  
-	    engTree.setEnableDD(false);  
-	    engTree.setContainerScroll(true);  
-	    engTree.setAutoScroll(true);  
-	    engTree.setRootVisible(false);  
-	    engTree.setFrame(true);
 
 	    // Find out where the host page wants the button.
 	    //
@@ -237,14 +45,13 @@ public class WebInterface implements EntryPoint {
 
 	    // Find out where the host page wants the tree view.
 	    //
-	    RootPanel treeViewSlot = RootPanel.get("tree");
+	    RootPanel treeViewSlot = RootPanel.get("engineTree");
 	    if (treeViewSlot == null) {
 	      Window.alert("Please define a container element whose id is 'tree'");
 	      return;
 	    }
 
 	    // Add both widgets.
-	    //
 	    searchButtonSlot.add(searchButton);
 	    treeViewSlot.add(engTree);
 	  }
