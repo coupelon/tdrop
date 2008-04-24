5f9abffbf28e4e8aa220934cc650bbe6598e200d
coupelon <coupelon@e14bd3a3-8b4b-0410-91c7-317e00807df5>
2008-04-24 16:04:31 +0000

    Huge comments addition, in every class of the project. Comments are Doxygen compliant.

--- trunk/tdwrapper/pageParser.h
+++ trunk/tdwrapper/pageParser.h
@@ -1,4 +1,4 @@
-/** 
+/*
 Copyright 2008 Olivier COUPELON
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -27,8 +27,46 @@ See the License for the specific language governing permissions and limitations 
 using namespace std;
 
 class pageParser {
+
+public:
+  /**
+   * @param e The name of the engine to treat
+   * @param ler A pointer to the query parameters. Will alse be used to store the results.
+   * @param mut The mutex to use to update the results.
+   * @param t the glocal engine parameters (proxies, etc...).
+   */
+	pageParser(const string & e, engineResults *ler, threadPool<pageParser> *mut, tdParam *t);
+	pageParser(const pageParser &);
+	~pageParser();
+	
+	/**
+   * This method is used to handle the whole engine parsing.
+   */
+	void startParsing();
+	
+	/**
+   * A convenience function for threads manageability
+   */
+	void operator()() { startParsing(); }
+	
+	/**
+	 * Set the global cancellation value.
+	 * @param val If set to true the parsing will end as soon as it can without storing its results
+	 */
+	void setAbort(bool val = true) { global_results->setAbort(val); }
+	
+  bool getAbort() { return *global_results->getAbort(); }
+	vector<row> & getResults();
+	string getEngineName() { return engname; }
+  
 private:
-	xmlEngine *eng;
+	bool initParse();
+	void doParse();
+	void closeParse(bool = true);
+	row generateRow(regExp &);
+	address getNextAddress();
+	
+  xmlEngine *eng;
 	string query;
 	string engname;
 	list<address> pages;
@@ -38,23 +76,7 @@ private:
 	threadPool<pageParser> *results_lock;
 	unsigned long limit;
 	getHttp gh;
-    tdParam *tdp;
-
-	bool initParse();
-	void doParse();
-	void closeParse(bool = true);
-	row generateRow(regExp &);
-	address getNextAddress();
-public:
-	pageParser(const string &, engineResults *, threadPool<pageParser> *, tdParam *);
-	pageParser(const pageParser &);
-	~pageParser();
-	void startParsing();
-	void operator()() { startParsing(); }
-	vector<row> & getResults();
-	string getEngineName() { return engname; }
-	void setAbort(bool val = true) { global_results->setAbort(val); }
-  bool getAbort() { return *global_results->getAbort(); }
+  tdParam *tdp;
 };
 
 #endif
