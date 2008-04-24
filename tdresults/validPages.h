1d6f4e5669667d7b6620be551d4be5e56cb49fb6
coupelon <coupelon@e14bd3a3-8b4b-0410-91c7-317e00807df5>
2008-04-24 16:04:36 +0000

    Huge comments addition, in every class of the project. Comments are Doxygen compliant.

--- tdresults/validPages.h
+++ tdresults/validPages.h
@@ -1,4 +1,4 @@
-/** 
+/*
 Copyright 2008 Olivier COUPELON
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -16,14 +16,28 @@ See the License for the specific language governing permissions and limitations 
 using namespace std;
 
 class validPages {
-private:
-    string url;
-    bool *results;
-    tdParam *tdp;
 public:
-    validPages(string u,bool *r, tdParam *t);
-    void validate();
+	/**
+	 * @param u The url to check
+	 * @param r A boolean pointer which will be set according to the url validity
+	 * @param t The Teardrop's parameters
+	 */
+  validPages(string u,bool *r, tdParam *t);
+  
+  /**
+   * Starts the validation
+   */
+  void validate();
+  
+  /**
+   * A convenience method to allow for threads usage
+   */
 	void operator()() { validate(); }
+	
+private:
+  string url;
+  bool *results;
+  tdParam *tdp;
 };
 
 #endif
